d322717896cab141c97d047f4a25d5762ab43e64
Anna Scheuler <scheul93@gmail.com>
2018-04-12 17:15:43 -0500

    search bar component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: this.props.value
+        }
+    }
+
+    onInputChange(e) {
+        this.setState({inputValue: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="search-box">
+                <ZipCodeField zipCode={this.state.inputValue} onInputChange={this.onInputChange.bind(this)} />
+                <Button isDisabled={!this.state.inputValue}/>
+            </div>
+        )
+    }
+}
+
+const ZipCodeField = props => {
+    return (
+        <div className="field">
+            <label className="label" htmlFor="zipCode">Zip Code</label>
+            <input className="input" name="zipCode" id="zipCode" pattern="[0-9]{5}" onChange={props.onInputChange} />
+        </div>
+    )
+}
+
+const Button = props => <button className="button" disabled={props.isDisabled}>Search</button>
+
+export default Search;
